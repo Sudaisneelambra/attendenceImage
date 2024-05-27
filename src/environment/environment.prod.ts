57f404e60c7fbd422ae2f0c000081cd68da116c6
Sudaisneelambra <sudaisanuneelambra21@gmail.com>
2024-05-27 10:37:17 +0530

    environment deleted

--- src/environment/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    api: 'http://localhost:3000',
-    socketApi: 'http://localhost:3020'
-}
--- src/environment/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    api: 'http://localhost:3000',
-    socketApi: 'http://localhost:3020'
-}
